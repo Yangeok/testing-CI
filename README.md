006c70856419071db48f06daadaa17c362362879
Yangwook <wooky92@naver.com>
2019-04-07 21:43:34 +0900

    -

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - yml파일에 작성한 유튜브, 블로그 크롤링하기
 - 크롤링한 데이터를 db에 집어넣기
 - 크롤링한 데이터를 api로 제공하기
+
+##
+
+![](https://res.cloudinary.com/yangeok/image/upload/v1554476890/1.jpg)
--- src/app.js
+++ src/app.js
@@ -2,7 +2,15 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('koa2-cors');
 const json = require('koa-json');
+const logger = require('koa-logger');
 const bodyParser = require('koa-body');
+const mongoose = require('mongoose');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection err:'));
+db.once('open', () => {
+  console.log('connected');
+});
 
 const app = new Koa();
 const router = new Router();
@@ -13,6 +21,7 @@ router.use(api.routes());
 app
   .use(json())
   .use(cors())
+  .use(logger())
   .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods());
