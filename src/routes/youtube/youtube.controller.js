c3f5fdba8304e4a3cd5a07e46cf4775ea09bdd41
Yangwook <wooky92@naver.com>
2019-04-18 18:59:03 +0900

    Modify all routers

--- src/routes/youtube/youtube.controller.js
+++ src/routes/youtube/youtube.controller.js
@@ -1,29 +1,22 @@
 const Youtube = require('../../db/models/youtube');
 const YoutubeFeeds = require('../../db/models/youtubeFeeds');
+const { getYoutubeAuthors, getYoutubeFeeds } = require('../../utils/query');
+const { successMessage, errorMessage } = require('../../utils/response');
 
 exports.list = async ctx => {
-  const youtube = await Youtube.find()
-    .select('-_id name url desc')
-    .sort({ name: 'asc' });
-  ctx.body = youtube;
+  try {
+    ctx.body = successMessage('data', await getYoutubeAuthors());
+  } catch (e) {
+    ctx.status = 400;
+    ctx.body = errorMessage(e.message);
+  }
 };
 
 exports.feeds = async ctx => {
-  const pageSize = 100;
-  const page = ctx.request.query.page || 1;
-  const limit = ctx.request.query.limit || pageSize;
-
-  const options = {
-    page: page,
-    limit: limit,
-    sort: { pubDate: 'desc' }
-  };
   try {
-    const aggregate = YoutubeFeeds.aggregate();
-    const data = await YoutubeFeeds.aggregatePaginate(aggregate, options);
-    ctx.body = { success: true, data: data };
+    ctx.body = successMessage('data', await getYoutubeFeeds(ctx));
   } catch (e) {
     ctx.status = 400;
-    ctx.body = { success: false, message: e.message };
+    ctx.body = errorMessage(e.message);
   }
 };
--- src/utils/query.js
+++ src/utils/query.js
@@ -1,5 +1,7 @@
 const Blog = require('../db/models/blog');
 const BlogFeeds = require('../db/models/blogFeeds');
+const Youtube = require('../db/models/youtube');
+const YoutubeFeeds = require('../db/models/youtubeFeeds');
 
 exports.getBlogAuthors = async () => {
   const data = await Blog.find()
@@ -8,14 +10,33 @@ exports.getBlogAuthors = async () => {
   return data;
 };
 
+exports.getYoutubeAuthors = async () => {
+  const data = await Youtube.find()
+    .select('-_id name url desc')
+    .sort({ name: 'asc' });
+  return data;
+};
+
 exports.getBlogFeeds = async ctx => {
-  const options = {
+  let options = {
     page: ctx.params.page || 1,
     limit: ctx.params.limit || 100,
     sort: { pubDate: 'desc' }
   };
 
-  const aggregate = BlogFeeds.aggregate();
-  const data = await BlogFeeds.aggregatePaginate(aggregate, options);
+  const aggregate = YoutubeFeeds.aggregate();
+  const data = await YoutubeFeeds.aggregatePaginate(aggregate, options);
+  return data;
+};
+
+exports.getYoutubeFeeds = async ctx => {
+  let options = {
+    page: ctx.params.page || 1,
+    limit: ctx.params.limit || 100,
+    sort: { pubDate: 'desc' }
+  };
+
+  const aggregate = YoutubeFeeds.aggregate();
+  const data = await YoutubeFeeds.aggregatePaginate(aggregate, options);
   return data;
 };
